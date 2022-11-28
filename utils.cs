71f9615eb29454289e4068a66e100ff2ce2544cf
EdinaelsonGaleno <sedinaelson@gmail.com>
2022-11-28 19:54:27 -0300

    criar pasta para video

--- utils.cs
+++ utils.cs
@@ -22,6 +22,28 @@ public static class utils
         
     }
 
+    //fazer metodo para criar pasta com o nome do arquivo.
+    
+    public static void moveVideos(string[] archive, string destiny)
+    {
+
+        string[] video = archive;
+
+        if (!Directory.Exists(destiny))
+        {
+            Directory.CreateDirectory(destiny);
+        }
+        
+        for (int i = 0; i < archive.Length; i++)
+        {
+            var files = new FileInfo(archive[i]);
+            files.MoveTo(Path.Combine(destiny, files.Name));
+        }
+    }
+
+    /**
+     * function of videos*
+     */
     public static void moveprograms(string[] archive, string destiny)
     {
         if (!Directory.Exists(destiny))
